dbc10297fe47d87928326a369c6cbb4d5a30929c
Martez Reed <martez.reed@greenreedtech.com>
2022-03-12 11:40:38 -0600

    Update output address information

--- nightlightdns.go
+++ nightlightdns.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net"
 
 	"github.com/coredns/coredns/plugin"
 	"github.com/coredns/coredns/plugin/metrics"
@@ -36,17 +37,31 @@ type Nightlightdns struct {
 
 // ServeDNS implements the plugin.Handler interface. This method gets called when example is used
 // in a Server.
-func (e Nightlightdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
+func (n Nightlightdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	// This function could be simpler. I.e. just fmt.Println("example") here, but we want to show
 	// a slightly more complex example as to make this more interesting.
 	// Here we wrap the dns.ResponseWriter in a new ResponseWriter and call the next plugin, when the
 	// answer comes back, it will print "example".
 
+	var (
+		err error
+	)
+
 	// Debug log that we've have seen the query. This will only be shown when the debug plugin is loaded.
 	log.Debug("Received response")
 	state := request.Request{W: w, Req: r}
-	name := state.Name()
-	log.Info(name)
+	qname := state.Name()
+	log.Info(qname)
+	answers := []dns.RR{}
+
+	// check record type here and bail out if not A or AAAA
+	if state.QType() != dns.TypeA && state.QType() != dns.TypeAAAA {
+		// always fallthrough if configured
+		return plugin.NextOrFailure(n.Name(), n.Next, ctx, w, r)
+
+		// otherwise return SERVFAIL here without fallthrough
+		return dnserror(dns.RcodeServerFailure, state, err)
+	}
 
 	file, _ := ioutil.ReadFile("dns.json")
 
@@ -54,17 +69,36 @@ func (e Nightlightdns) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dn
 
 	_ = json.Unmarshal([]byte(file), &data)
 
+	outip := ""
 	for _, record := range data.Records {
 		log.Info(record.Ipaddress)
+		outip = record.Ipaddress
 	}
-	// Wrap.
-	pw := NewResponsePrinter(w)
+
+	answers = append(answers, &dns.A{
+		Hdr: dns.RR_Header{
+			Name:   qname,
+			Rrtype: dns.TypeA,
+			Class:  dns.ClassINET,
+			Ttl:    30,
+		},
+		A: net.IP(outip),
+	})
 
 	// Export metric with the server label set to the current server handling the request.
 	requestCount.WithLabelValues(metrics.WithServer(ctx)).Inc()
 
-	// Call next plugin (if any).
-	return plugin.NextOrFailure(e.Name(), e.Next, ctx, pw, r)
+	// create DNS response
+	m := new(dns.Msg)
+	m.SetReply(r)
+	m.Authoritative = true
+	m.Answer = answers
+
+	// send response back to client
+	_ = w.WriteMsg(m)
+
+	// signal response sent back to client
+	return dns.RcodeSuccess, nil
 }
 
 // Name implements the Handler interface.
@@ -85,4 +119,16 @@ func (r *ResponsePrinter) WriteMsg(res *dns.Msg) error {
 	log.Info("nightlightdns")
 	fmt.Println("nightlightdns")
 	return r.ResponseWriter.WriteMsg(res)
+}
+
+func dnserror(rcode int, state request.Request, err error) (int, error) {
+	m := new(dns.Msg)
+	m.SetRcode(state.Req, rcode)
+	m.Authoritative = true
+
+	// send response
+	_ = state.W.WriteMsg(m)
+
+	// return success as the rcode to signal we have written to the client.
+	return dns.RcodeSuccess, err
 }
